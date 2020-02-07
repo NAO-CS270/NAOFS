962ce17f716394f8f02dcd33111bc684171adbfa
Aarti Jivrajani <aartij17@gmail.com>
2020-02-06 16:18:15 -0800

    Added bread functionality

--- src/dsk/blkfetch.c
+++ src/dsk/blkfetch.c
@@ -7,3 +7,10 @@ disk_block* getDiskBlock(int blockNumber) {
 void writeDiskBlock(int blockNumber, disk_block* blockData) {
 	writeMemoryDiskBlock(blockNumber, blockData);
 }
+
+char* readDiskBlock(int blockNumber, size_t numBytesToRead) {
+    disk_block* metaBlock = getDiskBlock(blockNumber);
+    char *buffer;
+    memcpy(buffer, metaBlock->data, numBytesToRead);
+    return buffer;
+}
