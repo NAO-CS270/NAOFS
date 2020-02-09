4263d45fae416c76bb2e73a15936adf00353e1e6
Abtin Bateni <abtinbateni@gmail.com>
2020-02-09 12:20:21 -0800

    setting up gtest with CI

---
+++ src/example.cpp
@@ -0,0 +1,20 @@
+//
+// Created by Abtin Bateni on 2/9/20.
+//
+
+#include "example.h"
+
+double add_numbers(const double f1, const double f2)
+{
+    return f1 + f2;
+}
+
+double subtract_numbers(const double f1, const double f2)
+{
+    return f1 - f2;
+}
+
+double multiply_numbers(const double f1, const double f2)
+{
+    return f1 * f2;
+}
