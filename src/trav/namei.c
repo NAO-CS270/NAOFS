a8b722db8f905e57cae5598e32c7ebd96104a7ee
Aarti Jivrajani <aartij17@gmail.com>
2020-02-10 14:32:50 -0800

    Fixed compilation errors in trav module

--- src/trav/namei.c
+++ src/trav/namei.c
@@ -5,6 +5,48 @@
 #include <string.h>
 #include <ctype.h>
 
+/* Ensures that path has only alphanumeric, `_`, `-` or `/` characters. Terminates the path
+ * by null byte if it isn't. Returns final path length.
+ */
+size_t checkAndGetLen(char *path, size_t bufLen) {
+    size_t counter;
+    for (counter = 0 ; counter<bufLen ; counter++) {
+        if (isalnum(path[counter]) != 0) {
+            continue;
+        }
+        if (path[counter] == '_' || path[counter] == '-' || path[counter] == '/') {
+            continue;
+        }
+        if (path[counter] == '\0') {
+            return counter;
+        }
+    }
+    path[bufLen-1] = '\0';
+    return bufLen;
+}
+
+
+void operate(char *workingBuffer, inCoreiNode *workingINode) {
+    size_t iNodeNum = findINodeInDirectory(workingINode, workingBuffer);
+    iput(workingINode);
+    workingINode = iget(iNodeNum, 0);
+}
+
+
+size_t processNextLevel(char *path, char *workingBuffer, inCoreiNode *workingINode) {
+    size_t counter;
+    for (counter=0 ; ; counter++) {
+        if ((path[counter] == '/') || (path[counter] == '\0')) {
+            operate(workingBuffer, workingINode);
+            break;
+        }
+        workingBuffer[counter] = path[counter];
+        counter++;
+    }
+    return counter;
+}
+
+
 void getFileINode(char *path, size_t bufLen, inCoreiNode *iNodeData) {
 	size_t pathLen = checkAndGetLen(path, bufLen);
 	char *workingBuffer = (char *)malloc((pathLen + 1)*sizeof(char));
@@ -23,42 +65,4 @@ void getFileINode(char *path, size_t bufLen, inCoreiNode *iNodeData) {
 	}
 }
 
-size_t processNextLevel(char *path, char *workingBuffer, inCoreiNode *workingINode) {
-	size_t counter;
-	for (counter=0 ; ; counter++) {
-		if ((path[counter] == '/') || (path[counter] == '\0')) {
-			operate(workingBuffer, workingINode);
-			break;
-		}
-		workingBuffer[counter] = path[counter];
-		counter++;
-	}
-	return counter;
-}
-
-void operate(char *workingBuffer, inCoreiNode *workingINode) {
-	size_t iNodeNum = findINodeInDirectory(workingINode, workingBuffer);
-	iput(workingINode);
-	workingINode = iget(iNodeNum, 0);
-}
-
-/* Ensures that path has only alphanumeric, `_`, `-` or `/` characters. Terminates the path
- * by null byte if it isn't. Returns final path length.
- */
-void checkAndGetLen(char *path, size_t bufLen) {
-	size_t counter;
-	for (counter = 0 ; counter<bufLen ; counter++) {
-		if (isalnum(path[counter]) != 0) {
-			continue;
-		}
-		if (path[counter] == '_' || path[counter] == '-' || path[counter] == '/') {
-			continue;
-		}
-		if (path[counter] == '\0') {
-			return counter;
-		}
-	}
-	path[bufLen-1] = '\0';
-	return bufLen;
-}
 
