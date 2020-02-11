3146dbfb39497e1353aba98d114d2d2cad97cdf4
Aarti Jivrajani <aartij17@gmail.com>
2020-02-11 00:55:58 -0800

    add extern to the function signatures

--- include/dsk/free.h
+++ include/dsk/free.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include "inode/inCoreiNode.h"
 
-void blockFree(size_t blockNumber);
-void inodeBlocksFree(inCoreiNode *inode);
+extern void blockFree(size_t blockNumber);
+extern void inodeBlocksFree(inCoreiNode *inode);
 
 #endif //NAOFS_FREE_H
