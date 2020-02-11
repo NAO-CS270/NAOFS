008ad9aea1c98d5a06f4decb38194e78969e0db6
Aarti Jivrajani <aartij17@gmail.com>
2020-02-10 17:37:22 -0800

    update free interface

--- include/dsk/free.h
+++ include/dsk/free.h
@@ -2,7 +2,9 @@
 #define NAOFS_FREE_H
 
 #include <stdlib.h>
+#include "inode/inCoreiNode.h"
 
 void blockFree(size_t blockNumber);
+void inodeBlocksFree(inCoreiNode *inode);
 
 #endif //NAOFS_FREE_H
