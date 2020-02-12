dde99768c4de1d308d367b490c90dda6175f9f73
Kalyan Kumar <kalyankumar.garapaty@gmail.com>
2020-02-12 00:58:22 -0800

    Fix bmap and iget seg faults

--- src/incoreInodeOps/bmap.c
+++ src/incoreInodeOps/bmap.c
@@ -1,92 +1,66 @@
 #include "dsk/blkfetch.h"
+#include "incoreInodeOps/iNodeManager.h"
 #include "incoreInodeOps/bmap.h"
+#include "mandsk/params.h"
+#include "mkfs/metaBlocks.h"
+#include "dsk/alloc.h"
 
+#include <stdlib.h>
 
-// num of block addresses in indirect block
-#define blkAddrNos (BLOCK_SIZE / BLOCK_ADDRESS_SIZE)
-// max size mapped by all the direct blocks
-#define directBlkMaxSize  (BLOCK_SIZE * DIRECT_BLOCKS)
-// max size mapped by all the single indirect blocks
-#define singleIndirectBlkMaxSize (BLOCK_SIZE * blkAddrNos)
-// max size mapped by all the double indirect blocks
-#define doubleIndirectBlkMaxSize (BLOCK_SIZE * blkAddrNos * blkAddrNos)
+typedef freeDiskListBlock indirectBlock;
 
-bmapResponse* bmap(inCoreiNode* inode, size_t offset) {
-    // allocate size for bmap response
-    bmapResponse* bmapResp = (bmapResponse*)malloc(sizeof(bmapResponse));
-    bmapResp->byteOffsetInBlock = offset % BLOCK_SIZE;
-    size_t numBytesToRead = BLOCK_SIZE - bmapResp->byteOffsetInBlock;
-    bmapResp->ioBytesInBlock = numBytesToRead;
-    //TODO: see if we need read ahead block number
+size_t processAndGetDiskBlock(blkTreeOffset *indirectionOffsets, size_t startBlockNum) {
+	size_t indirectBlockIndex = startBlockNum;
+	disk_block* dataBlock = (disk_block*)malloc(BLOCK_SIZE);
+	indirectBlock* workingData = (indirectBlock*)malloc(sizeof(indirectBlock));
 
-    // the block number in which offset resides
-    size_t blockNum;
+	size_t counter = 0;
+	while(counter < indirectionOffsets->offsetIndirection) {
+		getDiskBlock(indirectBlockIndex, dataBlock);
+		makeFreeDiskListBlock(dataBlock, workingData);
 
-    // indirection level
-    int indirectionLevel = 0;
-    // 1 if fetch block number from iNode
-    int inodeBlkFlg = 1;
+		indirectBlockIndex = (workingData->blkNos)[indirectionOffsets->offsets[counter + 1]];
+		counter++;
+	}
 
-    // determing the level of indirection
-    if ((offset >= directBlkMaxSize) && (offset < singleIndirectBlkMaxSize))
-        indirectionLevel = 1;
-    else if ((offset >= singleIndirectBlkMaxSize) && (offset < doubleIndirectBlkMaxSize))
-        indirectionLevel = 2;
-    else {
-        // TODO:handle block not found error
-        return NULL;
-    }
+	free(indirectionOffsets);
+	free(dataBlock);
+	free(workingData);
 
-    size_t newOffset = offset;
-    int i = indirectionLevel;
-    size_t blkSize = BLOCK_SIZE;
+	return indirectBlockIndex;
+}
 
-    while (i >= 0) {
-        // calculating the index
-        size_t index = newOffset / blkSize;
+void createBMapResponse(size_t blockNum, size_t offset, bmapResponse *response) {
+	response->blockNumber = blockNum;
+	response->byteOffsetInBlock = offset;
+	response->bytesLeftInBlock = BLOCK_SIZE - offset;
+}
 
-        // fetching block number from inode
-        if (inodeBlkFlg) {
-            if (indirectionLevel == 0)
-                blockNum = inode->dataBlockNums[index];
-            else if (indirectionLevel == 1)
-                blockNum = inode->dataBlockNums[11];
-            else if (indirectionLevel == 2)
-                blockNum = inode->dataBlockNums[12];
-            inodeBlkFlg = 0;
-        } else if (indirectionLevel != 0) {
-            // fetch the indirect block
-            disk_block* metaBlock = (disk_block*)malloc(BLOCK_SIZE);
-            metaBlock = getDiskBlock(blockNum, metaBlock);
-            indirectBlock* diskBlock = (indirectBlock*)malloc(sizeof(indirectBlock));
-            diskBlock = makeFreeDiskListBlock (metaBlock, diskBlock);
+void bmap(inCoreiNode* iNode, size_t offset, bmapResponse *response) {
+	size_t fileSize = iNode->size;
+	if (offset > fileSize) {
+		free(response);
+		response = NULL;
+		return ;
+	}
+	if (offset == fileSize && (offset%BLOCK_SIZE) == 0) {
+		size_t newBlock = blockAlloc();
+		insertDataBlockInINode(iNode, newBlock);
+		createBMapResponse(newBlock, 0, response);
+		return ;
+	}
+	
+	blkTreeOffset *indirectionOffsets = (blkTreeOffset *)malloc(sizeof(blkTreeOffset));
+	calculateOffset(offset, indirectionOffsets);
 
-            // read the block number
-            blockNum = diskBlock->blkNos[index];
+	size_t dataBlockNum;
+	if (indirectionOffsets->offsetIndirection == DIRECT) {
+		dataBlockNum = indirectionOffsets->offsets[0];
+	}
+	else {
+		size_t iNodeBlockIndex = DIRECT_BLOCK_LIMIT + indirectionOffsets->offsetIndirection;
+		dataBlockNum = processAndGetDiskBlock(indirectionOffsets, iNode->dataBlockNums[iNodeBlockIndex]);
+	}
 
-            free(diskBlock);
-            free(metaBlock);
-        }
-
-        // if reached final block number, return
-        if (i == 0) {
-            bmapResp->blockNumber = blockNum;
-            return bmapResp;
-        } else if (i == 1) {
-            // resetting offset to start from beginning of single indirect block
-            if (indirectionLevel == 1)
-                newOffset = newOffset - directBlkMaxSize;
-            else if (indirectionLevel == 2) {
-                // offset - single indirect blocks covered till now
-                newOffset = newOffset - singleIndirectBlkMaxSize * index;
-                blkSize = BLOCK_SIZE;
-            }
-        } else if (i == 2) {
-            // resetting offset to start from beginning of double indirect block
-            newOffset = newOffset - singleIndirectBlkMaxSize;
-            blkSize = BLOCK_SIZE * blkAddrNos;
-        }
-
-        i--;
-    }
+	createBMapResponse(dataBlockNum, offset%BLOCK_SIZE, response);
 }
