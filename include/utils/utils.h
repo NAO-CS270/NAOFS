59b8149a4903065e2bfbbb9d31bfc9e3bc412aff
Aarti Jivrajani <aartij17@gmail.com>
2020-02-12 00:21:55 -0800

    define max and min funcs

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -6,6 +6,15 @@
         do { fprintf(stderr, "%s:%d:%s(): \n" fmt, __FILE__, \
                                 __LINE__, __func__, __VA_ARGS__); } while (0)
 
+#define max(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a > _b ? _a : _b; })
+
+#define min(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
 
 extern int string_split(char*, char *[], const char*);
 extern char* getParentDirectory(const char* path);
