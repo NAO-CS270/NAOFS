d01b2b86dd7b677eb13a6acaed55aa5ff4c21d24
Aarti Jivrajani <aartij17@gmail.com>
2020-02-18 12:29:52 -0800

    removed debug prints

--- src/dsk/blkfetch.c
+++ src/dsk/blkfetch.c
@@ -3,12 +3,12 @@
 #include <stdio.h>
 
 disk_block* getDiskBlock(size_t blockNumber, disk_block *blockData) {
-	printf("Got a read request for block number %ld\n", blockNumber);
+	//printf("Got a read request for block number %ld\n", blockNumber);
 	return fetchMemoryDiskBlock(blockNumber, blockData);
 }
 
 void writeDiskBlock(size_t blockNumber, disk_block* blockData) {
-	printf("Got a write request for block number %ld\n", blockNumber);
+	//printf("Got a write request for block number %ld\n", blockNumber);
 	writeMemoryDiskBlock(blockNumber, blockData);
 }
 
