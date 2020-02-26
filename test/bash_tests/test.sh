e4a11f2e7dad6ddbe80e20a0c66a5cd650a187b1
Abtin Bateni <abtinbateni@gmail.com>
2020-02-25 16:24:52 -0800

    add create dir test

--- test/bash_tests/test.sh
+++ test/bash_tests/test.sh
@@ -2,10 +2,20 @@
 
 testChangeDir() {
   pwd1=$(pwd)
-  cd fsRoot || fail "cd in error"
-  cd ../ || fail "cd back error"
+  cd fsRoot || fail "cd-in error"
+  cd ../ || fail "cd-back error"
   pwd2=$(pwd)
 
+  assertEquals "$pwd1" "$pwd2"
+}
+
+testCreateDir() {
+  cd fsRoot || fail "cd-in error"
+  mkdir test || fail "mkdir error"
+  if [ ! -d test ]; then
+    # Control will enter here if $DIRECTORY doesn't exist.
+    fail "dir create error"
+  fi
   assertEquals "$pwd1" "$pwd2"
 }
 
