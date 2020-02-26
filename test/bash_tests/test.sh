f76fd6c0ed2b7cf2ebb82dd3dfe1e22858a6763a
Abtin Bateni <abtinbateni@gmail.com>
2020-02-25 16:20:14 -0800

    add cd test

--- test/bash_tests/test.sh
+++ test/bash_tests/test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+testChangeDir() {
+  pwd1=$(pwd)
+  cd fsRoot || fail "cd in error"
+  cd ../ || fail "cd back error"
+  pwd2=$(pwd)
+
+  assertEquals "$pwd1" "$pwd2"
+}
+
 testEquality() {
 	assertEquals 1 1
 }
