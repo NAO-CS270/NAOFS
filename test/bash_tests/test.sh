9ab54f84a2e11a201b753c6b2b5a25cf314418ff
Abtin Bateni <abtinbateni@gmail.com>
2020-02-25 16:44:17 -0800

    fix tests

--- test/bash_tests/test.sh
+++ test/bash_tests/test.sh
@@ -5,7 +5,6 @@ testChangeDir() {
   cd fsRoot || fail "cd-in error"
   cd ../ || fail "cd-back error"
   pwd2=$(pwd)
-
   assertEquals "$pwd1" "$pwd2"
 }
 
@@ -20,20 +19,15 @@ testCreateDir() {
 }
 
 testListDir() {
-  cd fsRoot || fail "cd-in error"
-  mkdir test || fail "mkdir error"
   lsResult=$(ls)
-
   assertEquals "$lsResult" "test"
 }
 
 testCdToCreatedDir() {
-  cd fsRoot || fail "cd-in error"
-  mkdir test || fail "mkdir error"
   pwd1=$(pwd)
   cd test || fail "cd to created dir error"
   pwd2=$(pwd)
-
+  cd ../ || fail "cd-back error"
   assertEquals "$pwd1/test" "$pwd2"
 }
 
