b241d8bb2399fe336d472e61200218e0ddc046f7
Abtin Bateni <abtinbateni@gmail.com>
2020-02-29 16:46:22 -0800

    add tests

--- test/runner.c
+++ test/runner.c
@@ -1,16 +1,7 @@
-#include <stdio.h>
-
-#define CTEST_MAIN
-
-// uncomment lines below to enable/disable features. See README.md for details
-#define CTEST_SEGFAULT
-//#define CTEST_NO_COLORS
-#define CTEST_COLOR_OK
-
-#include "../include/ctest.h"
+#include "test/test_utils"
 
 int main(int argc, const char *argv[]) {
-    int result = ctest_main(argc, argv);
+    test_utils_runner();
 
     return result;
 }
--- test/utils/test_utils.c
+++ test/utils/test_utils.c
@@ -1,32 +1,38 @@
 #include "ctest.h"
 #include "utils/utils.h"
 
-CTEST(stringSplit, emptyTokens) {
+void stringSplit_emptyTokens() {
     char str[] = "aaa";
     char *tokens[4];
-    string_split(str, tokens, "a");
+//    string_split(str, tokens, "a");
 
     for (int i = 0; i < 4; ++i)
         ASSERT_STR(tokens[i], "");
 }
 
-CTEST(stringSplit, singleToken) {
+void stringSplit_singleToken() {
     char str[] = "aaa";
     char *tokens[4];
-    string_split(str, tokens, "b");
+//    string_split(str, tokens, "b");
 
     ASSERT_STR(tokens[0], "aaa");
     for (int i = 1; i < 4; ++i)
         ASSERT_STR(tokens[i], "");
 }
 
-CTEST(stringSplit, singleToken) {
+void stringSplit_singleToken2() {
     char str[] = "baa";
     char *tokens[4];
-    string_split(str, tokens, "b");
+//    string_split(str, tokens, "b");
 
     ASSERT_STR(tokens[0], "");
     ASSERT_STR(tokens[1], "aa");
     for (int i = 2; i < 4; ++i)
         ASSERT_STR(tokens[i], "");
+}
+
+void test_utils_runner() {
+    stringSplit_emptyTokens();
+    stringSplit_singleToken();
+    stringSplit_singleToken2();
 }
