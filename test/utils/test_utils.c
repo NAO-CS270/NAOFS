dd3d7c947ddf80cc227c10dca29d357e0062778b
Abtin Bateni <abtinbateni@gmail.com>
2020-02-29 17:44:04 -0800

    finish test framework

--- test/utils/test_utils.c
+++ test/utils/test_utils.c
@@ -1,39 +1,35 @@
-//#include "ctest.h"
 #include "utils/utils.h"
+#include "naotest.h"
 #include <stdio.h>
-#include <assert.h>
 
 void stringSplit_emptyTokens() {
     char str[] = "aaa";
     char *tokens[4];
-//    string_split(str, tokens, "a");
+    string_split(str, tokens, "a");
 
-//    for (int i = 0; i < 4; ++i)
-//        ASSERT_STR(tokens[i], "");
+    for (int i = 0; i < 4; ++i)
+        ASSERT_STR(tokens[i], "");
 }
 
 void stringSplit_singleToken() {
     char str[] = "aaa";
     char *tokens[4];
-    printf("TESTIIIIIIIIIIIING!");
-    int a = 0;
-    assert(a == 8);
-//    string_split(str, tokens, "b");
+    string_split(str, tokens, "b");
 
-//    ASSERT_STR(tokens[0], "aaa");
-//    for (int i = 1; i < 4; ++i)
-//        ASSERT_STR(tokens[i], "");
+    ASSERT_STR(tokens[0], "aaa");
+    for (int i = 1; i < 4; ++i)
+        ASSERT_STR(tokens[i], "");
 }
 
 void stringSplit_singleToken2() {
     char str[] = "baa";
     char *tokens[4];
-//    string_split(str, tokens, "b");
+    string_split(str, tokens, "b");
 
-//    ASSERT_STR(tokens[0], "");
-//    ASSERT_STR(tokens[1], "aa");
-//    for (int i = 2; i < 4; ++i)
-//        ASSERT_STR(tokens[i], "");
+    ASSERT_STR(tokens[0], "");
+    ASSERT_STR(tokens[1], "aa");
+    for (int i = 2; i < 4; ++i)
+        ASSERT_STR(tokens[i], "");
 }
 
 void test_utils_runner() {
